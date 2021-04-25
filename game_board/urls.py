47b3f7641a3693b245c8a2777a8ac03fe7df09cb
jgold3 <32280330+jgold3@users.noreply.github.com>
2021-04-25 12:54:33 -0400

    Configured URLs for ant actions
    These can now be used by front end devs and testing via requests

--- game_board/urls.py
+++ game_board/urls.py
@@ -23,4 +23,10 @@ urlpatterns = [
     path('llist_api', llist_api.api_overview, name='llist-game-board-api_overview'),
     path('llist_api/start_game/<str:difficulty>/<str:player_ids>/<str:data_structures>', llist_api.start_game, name='llist-game-board-start_game'),
     path('llist_api/board/<str:game_id>', llist_api.board, name='llist-game-board-game_status'),
+
+    path('llist_api/dig_tunnel/<str:id>/<str:origin>/<str:destination>', llist_api.dig_tunnel, name='llist-game-board-dig_tunnel'),
+    path('llist_api/dig_chamber/<str:id>/<str:origin>/<str:move_ant>', llist_api.dig_chamber, name='llist-game-board-dig_chamber'),
+    path('llist_api/fill_chamber/<str:id>/<str:origin>/<str:to_fill>', llist_api.fill_chamber, name='llist-game-board-fill_chamber'),
+    path('llist_api/spawn_ant/<str:id>', llist_api.spawn_ant, name='llist-game-board-spawn_ant'),
+    path('llist_api/forage/<str:id>/<str:difficulty>/<str:dest>', llist_api.forage, name='llist-game-board-forage'),
 ]
